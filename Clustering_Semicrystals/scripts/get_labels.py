7d4551a64231288218951ca47c6a39239cf01db2
bowenal91 <bowenal91@gmail.com>
2019-06-19 14:18:57 -0500

    Dirichlet process model works

---
+++ Clustering_Semicrystals/scripts/get_labels.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sys
+import os
+import math
+
+molySize = 7
+
+data = np.loadtxt('input.xyz')
+labels = np.loadtxt("labels.txt")
+
+output = open("clusters.xyz","w")
+
+els = ["H","C","N","O","S","F","P","Cl"]
+
+output.write(str(len(data))+"\n\n")
+j = 0
+for i in range(len(data)):
+    current_type = int(labels[j])%len(els)
+    output.write(str(els[current_type])+"\t"+str(data[i][1])+"\t"+str(data[i][2])+"\t"+str(data[i][3])+"\n")
+    if (i+1)%molySize == 0:
+        j += 1
+
+output.close()
